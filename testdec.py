8f2ca5fffee8e83326a9589516547f258cbadf4b
aliarefi1993 <arefi_ali90@yahoo.com>
2021-09-03 03:38:43 +0430

    first commit

---
+++ testdec.py
@@ -0,0 +1,16 @@
+
+def logged(func):
+    def wrapper(*args, **kwargs):
+        print(f"you called {func.__name__}({args}, {kwargs})")
+        value = func(*args, **kwargs)
+        print(f"it return {value}")
+        return value
+    return wrapper
+
+@logged
+def func(*args, **kwargs):
+    return 3 + len(args) + len(kwargs)
+
+print(func(1,2,3,5,6,9,6,6,0, a=4))
+    
+    
